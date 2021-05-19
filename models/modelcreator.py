db3a276dfd008abb034d8b7c82de29bba5d00ced
henri849 <70386636+henri849@users.noreply.github.com>
2021-05-18 17:40:10 -0700

    cleaned stuff up

--- models/modelcreator.py
+++ models/modelcreator.py
@@ -5,19 +5,6 @@ ans = open("output.txt","r")
 ans = json.loads(ans.read())
 
 [[x_train, x_test], [y_train, y_test]] = ans
-#print(len(x_train[0]),"YYYYYYYYYYYYYYY")
-# model = tf.keras.models.Sequential([
-#   tf.keras.layers.Input(shape=(4,)),
-#   tf.keras.layers.Dense(48, activation='relu'),
-#   tf.keras.layers.Dropout(0.2),
-#   tf.keras.layers.Dense(2, activation='softmax')
-# ])
-# x_train=np.asarray(x_train).astype(np.float32)
-# x_test=np.asarray(x_test).astype(np.float32)
-# tf.convert_to_tensor(x_train)
-# tf.convert_to_tensor(x_test)
-# tf.convert_to_tensor(y_train)
-# tf.convert_to_tensor(y_test)
 
 x_train = tf.ragged.constant(x_train).to_tensor()
 x_test = tf.ragged.constant(x_test).to_tensor()
@@ -26,9 +13,7 @@ y_test = tf.ragged.constant(y_test).to_tensor()
 print(x_train)
 model = tf.keras.Sequential([
     tf.keras.layers.Input(252,),
-#     tf.keras.layers.Input(252),
     tf.keras.layers.Dense(48, activation='relu'),
-    #tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
